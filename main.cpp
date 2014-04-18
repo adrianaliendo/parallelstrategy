cfc9f086336fa442b3598a43eb21e1116c33a159
Adriana Liendo <adriana.liendo@gmail.com>
2014-04-18 14:12:17 -0430

    Utilizando el switch para determinar el tipo de estrategia a usar

--- main.cpp
+++ main.cpp
@@ -42,6 +42,12 @@ void sumagpu(int IA, int KK, int DT, int *TMX1, int idn){
  * se debe setear la estrategia
 */
 int main(int argc, char** argv) {
+    char op='n';
+    /*cout << "argc:" << argc << endl;
+    if (argc>1 && argv[1]=="op")
+        op=(char) argv[2];*/
+    
+    
     MPI::Init(argc,argv);
     MPI::COMM_WORLD.Set_errhandler(MPI::ERRORS_THROW_EXCEPTIONS);
     
@@ -49,6 +55,7 @@ int main(int argc, char** argv) {
         int idnode = MPI::COMM_WORLD.Get_rank();
         cout << idnode << endl;
 
+        //Datos asociados al problema        
         void (*para_determinar_estrategia)(int IA, int KK, int DT, int *TMX1, int idn);
         int IA=1024;//*1024; //Pendiente con el tamanio 1024*1024 que es muy grande
         int TMX1[IA];
@@ -64,7 +71,8 @@ int main(int argc, char** argv) {
 
 
         //Defino el objeto que hara uso de la estrategia seleccionada
-        inicio=MPI::Wtime();
+        //Primera version
+        /*inicio=MPI::Wtime();
         para_determinar_estrategia=suma;
         ejecucion auxiliar(&muestraMPI); //Lo inicializo con la version para MPI
         auxiliar.Ejecuta(para_determinar_estrategia,IA,KK,DT,TMX1);
@@ -89,7 +97,32 @@ int main(int argc, char** argv) {
             auxiliar.Ejecuta(para_determinar_estrategia,IA,KK,DT,TMX1);
             final=MPI::Wtime();
             cout << "Ejecutado (CUDA) en:" << (final - inicio)*1000 << ": ms" << endl;
+        }*/
+        
+        inicio=MPI::Wtime();
+        ejecucion auxiliar(&muestraOpenMP); //Como por defecto
+        switch (op){
+            case 'o':   para_determinar_estrategia=suma;
+                        auxiliar.setEjecutor(&muestraOpenMP); //Lo inicializo con la version para MPI
+                        break;
+                        
+            case 'c':   para_determinar_estrategia=sumagpu;
+                        auxiliar.setEjecutor(&muestraCUDA); //Lo inicializo con la version para MPI
+                        break;
+                        
+            case 'n':
+            case 'm':
+            default:    para_determinar_estrategia=suma;
+                        auxiliar.setEjecutor(&muestraMPI); //Lo inicializo con la version para MPI
+                        break;        
         }
+        auxiliar.Ejecuta(para_determinar_estrategia,IA,KK,DT,TMX1);
+        final=MPI::Wtime();
+        if(idnode==0){
+            cout << "Ejecutado (" << op << ") en:" << (final - inicio)*1000 << ": ms" << endl;
+        }
+                
+        
 
     } catch(MPI::Exception e) {
         cout << "MPI ERROR: " << e.Get_error_code() << " - " << e.Get_error_string() << std::endl;
